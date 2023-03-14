0f796e1f988cf49a752b4166cf44ee7eba15584f
Tayyab Nisar <tayyabnisar@edraaksystems.com>
2023-03-14 15:20:55 +0500

    changes

--- frontend/src/redux/authSlice/apiCalls.js
+++ frontend/src/redux/authSlice/apiCalls.js
@@ -8,10 +8,11 @@ export const login = async (payload, dispatch) => {
 	dispatch(loginStart());
 	try {
 		dotenv.config();
+		const url = "https://weak-puce-kangaroo-cap.cyclic.app/api/login";
 
 		// const url = "http://localhost:8080/api/login";
 		console.log("api-----aaaa", process.env.REACT_APP_API_URL)
-		const url = `${process.env.REACT_APP_API_URL}` + "/login"
+		// const url = `${process.env.REACT_APP_API_URL}` + "/login"
 		const { data } = await axios.post(url, payload);
 
 		const decodeData = jwt_decode(data.data);
